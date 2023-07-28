96c071efb54c5e5c723474ea8f14c295c6a5da24
Andy Uyeda <andyjuyeda2@gmail.com>
2023-07-28 13:19:43 -0700

    Style buttons Operators now evaluate when selected after previous equation

--- main.js
+++ main.js
@@ -10,59 +10,55 @@ let operatorValue = "";
 let inputB = "";
 let operatorSelected = false;
 
-const screenText = document.querySelector('.screen-text');
+const screenText = document.querySelector(".screen-text");
 
-const numberButtons = document.querySelectorAll('.number');
+const numberButtons = document.querySelectorAll(".number");
 numberButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        if (!operatorSelected) {
-            inputA += button.dataset.value;
-            screenText.textContent = inputA;
-        } else {
-            inputB += button.dataset.value;
-            screenText.textContent = inputB;
-        }
-    });
+  button.addEventListener("click", () => {
+    if (!operatorSelected) {
+      inputA += button.dataset.value;
+      screenText.textContent = inputA;
+    } else {
+      inputB += button.dataset.value;
+      screenText.textContent = inputB;
+    }
+  });
 });
 
-const operatorButtons = document.querySelectorAll('.operator');
+const operatorButtons = document.querySelectorAll(".operator");
 operatorButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        button.classList.add('active');
-        operatorValue = button.dataset.value;
-        screenText.textContent = "";
-        operatorSelected = true;
-        operatorButtons.forEach((button) => {
-            button.disabled = true;
-        });
-    });
+  button.addEventListener("click", () => {
+    if (operatorSelected) {
+      inputA = operate(parseFloat(inputA), operatorValue, parseFloat(inputB));
+      screenText.textContent = inputA;
+      inputB = "";
+    }
+    operatorValue = button.dataset.value;
+    operatorSelected = true;
+  });
 });
-
 
 const operate = function (a, operator, b) {
   return operators[operator](a, b);
 };
 
-const equalsButton = document.querySelector('.equal');
-equalsButton.addEventListener('click', () => {
-    inputA = operate(parseFloat(inputA), operatorValue, parseFloat(inputB));
-    screenText.textContent = inputA;
-    inputB = "";
-    operatorButtons.forEach((button) => {
-        button.disabled = false;
-        button.classList.remove('active');
-    })
-})
+const equalsButton = document.querySelector(".equal");
+equalsButton.addEventListener("click", () => {
+  inputA = operate(parseFloat(inputA), operatorValue, parseFloat(inputB));
+  screenText.textContent = inputA;
+  inputB = "";
+  operatorSelected = false;
+});
 
-const clearButton = document.querySelector('.clear');
-clearButton.addEventListener('click', () => {
-    inputA = "";
-    operatorValue = "";
-    inputB = "";
-    operatorSelected = false;
-    screenText.textContent = "";
-    operatorButtons.forEach((button) => {
-        button.disabled = false;
-        button.classList.remove('active');
-    })
-})
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => {
+  inputA = "";
+  operatorValue = "";
+  inputB = "";
+  operatorSelected = false;
+  screenText.textContent = "";
+  operatorButtons.forEach((button) => {
+    button.disabled = false;
+    button.classList.remove("active");
+  });
+});
